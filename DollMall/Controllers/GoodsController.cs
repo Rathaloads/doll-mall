5bd2aaf366fd81d5576ca46de839ff6653cd511f
perry <1129816754@qq.com>
2020-05-19 15:14:46 +0800

    [init]初始化提交

---
+++ DollMall/Controllers/GoodsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DollMall.Controllers
+{
+    public class GoodsController : Controller
+    {
+        private readonly DataBase.DataContext _context;
+
+        public GoodsController(DataBase.DataContext context) {
+            _context = context;
+        }
+
+    }
+}
---
+++ DollMall/DataBase/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DollMall.DataBase
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Entity.T_Goods> T_Goods { get;set; }
+    }
+}
